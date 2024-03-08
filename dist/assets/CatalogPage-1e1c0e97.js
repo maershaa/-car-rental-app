d34bddbf19cd7ccb4d2a550c78f029187b353dc2
maershaa <valeria.work2022@gmail.com>
2024-03-08 16:02:44 +0200

    Revert "n"
    This reverts commit 130a4daed217dc9582de7ef536277ade28dff0e7.

--- dist/assets/CatalogPage-1e1c0e97.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-d7f9beac.js";const e=()=>a.jsx("div",{children:"CatalogPage"});export{e as default};
---
+++ dist/assets/DrinksPage-398b0f92.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-cf54eecb.js";const e=()=>r.jsx("div",{children:"DrinksPage"});export{e as default};
